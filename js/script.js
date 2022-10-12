77c269f5e68b633c0b9f5573e862bc6b102c86aa
Mattia-Rollo <ste.tia@hotmail.it>
2022-10-12 14:50:14 +0200

    add jsnack lista nome cognomi

--- js/script.js
+++ js/script.js
@@ -2,21 +2,43 @@
 Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 */
 
-const addInput = document.createElement('input');
 const wrapper = document.getElementById('wrapper');
 const btn = document.querySelector('button');
 
-wrapper.prepend(addInput);
 
-addInput.addEventListener('change', function() {
-    let value = parseInt(addInput.value);    console.log(addInput.value);
+for(let i = 0; i < 1 ; i++){
+    const addInput = document.createElement('input');
+    wrapper.prepend(addInput);
+}
+
+const primoSneak = wrapper.getElementsByTagName('input')[0];
+// wrapper.prepend(addInput);
+const cls = ['my-3','d-block'];
+primoSneak.classList.add(...cls);
+primoSneak.addEventListener('change', function() {
+    let value = parseInt(primoSneak.value);    
+    console.log(primoSneak.value);
     if (!(value % 2 == 0)){
-        console.log(addInput.value);
+        console.log(primoSneak.value);
         value++;
     }
 wrapper.getElementsByTagName('span')[0].innerHTML = value;
 })
 
+/*
+Generatore di “nomi cognomi” casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
 
-const wrapper2 = document.getElementById('wrapper2');
-// wrapper2.append(addInput);
+
+
+*/
+
+const listaNomi = ['mario', 'luca', 'giovanni'];
+const listaCognomi = ['rossi', 'capatonda','enstein'];
+
+for(let i = 0; i < listaNomi.length; i++){
+    let nomeCognome = listaNomi[i] + listaCognomi [i];
+    console.log(nomeCognome);
+}
+
+
+
